35bbe378309407e8781d124f7b8924bdeba242e4
kunalking123 <kunal.saw29@gmail.com>
2023-04-06 14:45:28 +0530

    update

---
+++ src/main/java/com/springboot/kafkaspringbootrealworldproject/KafkaSpringbootRealworldProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.kafkaspringbootrealworldproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringbootRealworldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringbootRealworldProjectApplication.class, args);
+	}
+
+}
